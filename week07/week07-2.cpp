edc4f66413b1af8588b25aca78e1ba803739863d
Lisa <91250177+happylisaya@users.noreply.github.com>
2021-10-28 17:42:00 +0800

    Add files via upload

---
+++ week07/week07-2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+        int n=3;
+
+        if(n>0){
+            printf("n大於0喔\n");
+        }
+
+        while (n>0){
+            printf("while(判斷n>0)有近來喔\n");
+        }
+
+}
+
